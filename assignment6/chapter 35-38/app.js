4fed371fdd848f620d3dd61d58339767ad73a61a
Rabiya <rabiamushtaqmithani@gmail.com>
2020-06-20 20:38:23 +0500

    update app.js

--- assignment6/chapter 35-38/app.js
+++ assignment6/chapter 35-38/app.js
@@ -1,78 +1,77 @@
-// task 1
-// function hello() {
-//     document.write("Hello World</br>");
-// }
-
-// hello();
-// hello();
-
-// task 2
+// // task 1
 // function date() {
 //     var now = new Date;
 //     document.write(now + "</br>");
 // }
+// date();
 
-// date();
-// date();
-// date();
 // document.write("</hr>");
 
-// //ques no 17
+
+
+// // task 2
 // function greeting(firstName, lastName) {
-//     var greet = firstName + lastName;
 //     var firstName = prompt("Enter your first name");
 //     var lastName = prompt("Enter your last name");
 //     document.write("Hello " + firstName + " " + lastName + "</br>");
 // }
 // greeting();
+
 // document.write("</hr>");
 
-// //ques no 18
-// function sum(num1, num2) {
-//     num1 = +prompt("Enter first number");
-//     num2 = +prompt("Enter second number");
-//     return num1 + num2;
+
+
+// // task 3
+// function sum(a, b) {
+//     a = +prompt("Enter first number");
+//     b = +prompt("Enter second number");
+//     return a + b;
 // }
 // var num1, num2;
-// var sumOf = sum(num1, num2);
-// document.write("Sum of 2 numbers is " + sumOf + "<br><hr/>");
+// document.write("Sum of 2 numbers is " + sum(num1, num2) + "<br><hr/>");
 
-// //ques no 19
-// function operation(n1, n2, sign) {
+
+
+// // task 4
+// document.write("<h2>Calculator:</h2>");
+
+// function calc(n1, n2, sign) {
 //     n1 = +prompt("Enter first number");
-//     n2 = +prompt("Enter secong number");
+//     n2 = +prompt("Enter second number");
 //     sign = prompt("Enter a sign for operation you want\n+ , - , * , / or %");
-//     switch (sign) {
-//         case "+":
-//             return n1 + n2;
-//             break;
-
-//         case "-":
-//             return n1 - n2;
-//             break;
-
-//         case "*":
-//             return n1 * n2;
-//             break;
-
-//         case "/":
-//             return n1 / n2;
-//             break;
-
-//         case "%":
-//             return n1 % n2;
-//             break;
-
-//         default:
-//             alert("something going wrong");
+//     if(sign=="+"){
+//         document.write("n1 is " + n1 + " n2 is " + n2 + "<br>");
+//         return n1+n2;
 //     }
+//     else if(sign=="-"){
+//         document.write("n1 is " + n1 + " n2 is " + n2 + "<br>");
+//         return n1-n2;
+//     }
+//     else if(sign=="*"){
+//         document.write("n1 is " + n1 + " n2 is " + n2 + "<br>");
+//         return n1*n2;
+//     }
+//     else if(sign=="/"){
+//         document.write("n1 is " + n1 + " n2 is " + n2 + "<br>");
+//         return n1/n2;
+//     }
+//     else if(sign=="%"){
+//         document.write("n1 is " + n1 + " n2 is " + n2 + "<br>");
+//         return n1%n2;
+//     }
+//     else{
+//         alert("You enteres wrong operation");
+//     }
+
 // }
 
 // var n1, n2, sign;
-// var result = operation(n1, n2, sign);
+// var result = calc(n1, n2, sign);
 // document.write("Your result is: " + result + "<br><hr/>");
 
-// //ques no 20
+
+
+// // task 5
 // function square(sqNum) {
 //     sqNum = +prompt("Enter any number to get it's square value");
 //     return sqNum * sqNum;
@@ -81,20 +80,100 @@
 // var numSquare = square(sqNum);
 // document.write("Square of your given num is: " + numSquare + "<br><hr/>");
 
-// //ques no 23
-// function hyp(base, perp) {
-//     function sqBsPr(value) {
-//         var value = value * value;
-//     }
 
-//     base = sqBsPr(base);
-//     perp = sqBsPr(perp);
+
+// // task 6
+// var num = +prompt("Enter any number to find its factorial");
+// function factor(fac) {
+//     if (fac < 0) {
+//         return -1;
+//     }
+//     else if (fac == 0) {
+//         return 1;
+//     }
+//     else {
+//         return (fac * factor(fac - 1));
+//     }
+// }
+// var fact = factor(num);
+// document.write("Factorial of "+num+ " is "+fact+"<br><hr>");
+
+
+
+// // task 7
+// var s = +prompt("Enter a number to start counting");
+// var e = +prompt("Enter a number to end counting");
+
+// document.write("Counting:<br>");
+// function count(start, end){
+//     for(var i = start; i<=end; i++){
+//         document.write(i+"<br>");
+//     }
+//     document.write("<hr>");
+// }
+// count(s,e);
+
+
+
+// // task 8
+// var base = +prompt("Enter number of Base");
+// var perp = +prompt("Enter number of Perpendicular");
+// function hyp(base, perp) {
+//     function sq(val){
+//         return val*val;
+//     }
+//     base = sq(base);
+//     perp = sq(perp);
 
 //     return base + perp;
 // }
-// var base = +prompt("Enter number of Base");
-// var perp = +prompt("Enter number of Perpendicular");
 
 // var hypotenuse = hyp(base, perp);
-// alert(hypotenuse);
-//document.write("Result of Hypotenuse is: "+ hypotenuse+ "<br><hr/>")
+// document.write("Value of Base is: "+base+ " and value of Perpendicular is: "+perp+"<br>");
+// document.write("Result of Hypotenuse is: "+ hypotenuse+ "<br><hr/>");
+
+
+
+// //task 9
+// document.write("Area of Rectangle:<br>");
+// var w = +prompt("Enter value of width");
+// var h = +prompt("Enter value of height");
+// function area(width, height){
+//     return width*height;
+// }
+// // i  - arguments as value
+// var area1 = area(3,5);
+// document.write("Area of Rectangle using arguments as value is "+area1+"<br>");
+
+// // ii - arguments as variables
+// var area2 = area(w,h);
+// document.write("Area of Rectangle using arguments as variables is "+area2+"<br><hr>");
+
+
+
+// // task 10
+// var str = prompt("Enter any word to check whether its palindrom or not")
+// function palindrome(str) {
+//     var mid = Math.floor((str.length) / 2);
+//     for (var i = 0; i < mid; i++) {
+//         if (str[i] !== str[(str.length) - 1 - i]) {
+//             document.write(str + " is not a Palindrom");
+//             break;
+//         }
+//         else {            
+//             document.write(str + " is a Palindrom");
+//             break;
+//         }
+//     }
+// }
+// palindrome(str);
+
+
+
+// task 11
+function capitalize(sentence) {
+    var splitSentence = sentence.split(" ");
+    for (var s = 0; s < splitSentence.length; s++) {
+        
+    }
+}
